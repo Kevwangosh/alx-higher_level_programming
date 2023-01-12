79410f8e0411dc5b2ce488ea8f84b395ef293a9a
Kevwangosh <kevinwangombe@gmail.com>
2023-01-12 12:16:46 +0000

    C

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -8,6 +8,6 @@ for digit1 in range(0, 10):
         if digit1 >= digit2:
             continue
         elif digit1 == 8 and digit2 == 9:
-	    print("{}{}".format(digit1, digit2))
+            print("{}{}".format(digit1, digit2))
 	else:
-	    print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(digit1, digit2), end=", ")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,6 +3,6 @@
 """Check for lowercase characters."""
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
-		 	return True;
-		 else:
-		 	return False;
+        return True
+    else:
+        return False
